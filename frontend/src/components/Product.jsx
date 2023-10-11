f156b707b2886ccd4821eab24df35d2fed07fae8
Muhmmad Imtinan Farooq <92246555+alidesu@users.noreply.github.com>
2023-10-11 07:54:31 +0500

    completed the product section along side the navbar.

---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import { Card } from "react-bootstrap";
+
+const Product = ({ product }) => {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </a>
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+        <Card.Text as="h3">
+            {product.price}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+Product.propTypes = {
+  product: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default Product;
