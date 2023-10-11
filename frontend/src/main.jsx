457839f3510388b6ae75da834751d62322ddcdf6
Muhmmad Imtinan Farooq <92246555+alidesu@users.noreply.github.com>
2023-10-11 08:33:31 +0500

    Added React-Routers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './assets/styles/index.css'
-import './assets/styles/bootstrap.custom.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./assets/styles/index.css";
+import "./assets/styles/bootstrap.custom.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={routes} />
+  </React.StrictMode>
+);
